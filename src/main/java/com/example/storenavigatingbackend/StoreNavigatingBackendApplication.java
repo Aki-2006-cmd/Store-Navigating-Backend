fd2ad4dbf3d265205f82e3b14acc6fb7303570c9
Akash Gamage <gamage.gdpa@gmail.com>
2026-09-16 16:00:27 +0530

    Initial commit - Spring Boot setup

---
+++ src/main/java/com/example/storenavigatingbackend/StoreNavigatingBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.storenavigatingbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StoreNavigatingBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StoreNavigatingBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/storenavigatingbackend/StoreNavigatingBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.storenavigatingbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StoreNavigatingBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
